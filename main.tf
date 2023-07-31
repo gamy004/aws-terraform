4de9f336f9536eeeb0295a6c5642286bb09af265
Wasuphat Pudjaika <narayutput@gmail.com>
2023-07-31 15:06:31 +0700

    update: default workspace

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region  = "ap-southeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-09964535fc01efa5f"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0d532cff718432c72"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "AWS_DEFAULT_REGION" {
+  description = "Value of the default region for the AWS cli"
+  type        = string
+  default       = "ap-southeast-1"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "Value of the Access Key ID for the AWS cli"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Value of the SECRET Access Key for the AWS cli"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
+}
