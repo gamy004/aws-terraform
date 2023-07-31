c3db72a2eefd7d1378bf9c101c1bc6f7375a039e
Wasuphat Pudjaika <narayutput@gmail.com>
2023-07-31 15:29:42 +0700

    update: remove aws env variable

--- variables.tf
+++ variables.tf
@@ -1,19 +1,3 @@
-variable "AWS_DEFAULT_REGION" {
-  description = "Value of the default region for the AWS cli"
-  type        = string
-  default       = "ap-southeast-1"
-}
-
-variable "AWS_ACCESS_KEY_ID" {
-  description = "Value of the Access Key ID for the AWS cli"
-  type        = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "Value of the SECRET Access Key for the AWS cli"
-  type        = string
-}
-
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
