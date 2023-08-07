1caf891d4bdd880536490dae19148249ea4f3079
Wasuphat Pudjaika <narayutput@gmail.com>
2023-08-07 16:42:44 +0700

    add: base GET method

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -26,12 +26,12 @@ resource "aws_api_gateway_resource" "proxy" {
   path_part   = "{proxy+}"
 }
 
-# resource "aws_api_gateway_method" "base" {
-#   rest_api_id   = aws_api_gateway_rest_api.api.id
-#   resource_id   = aws_api_gateway_resource.proxy.id
-#   http_method   = "GET"
-#   authorization = "NONE"
-# }
+resource "aws_api_gateway_method" "base" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
 
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
