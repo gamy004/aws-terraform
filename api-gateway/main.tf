cf295b4c53e143da0ab4cfa8df296b3e6e1e52ee
Gamiigo <narayutput@gmail.com>
2023-08-07 10:56:33 +0700

    add: attached policy to api gateway

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -54,6 +54,11 @@ data "aws_iam_policy_document" "api_access_policy" {
   }
 }
 
+resource "aws_api_gateway_rest_api_policy" "api_policy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  policy      = data.aws_iam_policy_document.api_access_policy.json
+}
+
 resource "aws_api_gateway_domain_name" "api_domain" {
   regional_certificate_arn = var.certificate_arn
   domain_name              = var.domain_name
