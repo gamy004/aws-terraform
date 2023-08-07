406cfb3cfd5c86df1229c5afac51f36377cbcf66
Wasuphat Pudjaika <narayutput@gmail.com>
2023-08-07 16:35:38 +0700

    update: disable base path

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -14,11 +14,11 @@ resource "aws_api_gateway_rest_api" "api" {
   tags = merge(var.tags, { Name = var.configs.name })
 }
 
-resource "aws_api_gateway_resource" "base" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = ""
-}
+# resource "aws_api_gateway_resource" "base" {
+#   rest_api_id = aws_api_gateway_rest_api.api.id
+#   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+#   path_part   = "/"
+# }
 
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
@@ -26,12 +26,12 @@ resource "aws_api_gateway_resource" "proxy" {
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "base" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "base" {
+#   rest_api_id   = aws_api_gateway_rest_api.api.id
+#   resource_id   = aws_api_gateway_resource.proxy.id
+#   http_method   = "GET"
+#   authorization = "NONE"
+# }
 
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
@@ -43,16 +43,16 @@ resource "aws_api_gateway_method" "proxy" {
   }
 }
 
-resource "aws_api_gateway_integration" "base" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.base.id
-  uri                     = "https://${var.configs.private_nlb_dns_name}/"
-  http_method             = aws_api_gateway_method.base.http_method
-  type                    = "HTTP_PROXY"
-  integration_http_method = "GET"
-  connection_type         = "VPC_LINK"
-  connection_id           = aws_api_gateway_vpc_link.vpc_link_to_nlb.id
-}
+# resource "aws_api_gateway_integration" "base" {
+#   rest_api_id             = aws_api_gateway_rest_api.api.id
+#   resource_id             = aws_api_gateway_resource.base.id
+#   uri                     = "https://${var.configs.private_nlb_dns_name}/"
+#   http_method             = aws_api_gateway_method.base.http_method
+#   type                    = "HTTP_PROXY"
+#   integration_http_method = "GET"
+#   connection_type         = "VPC_LINK"
+#   connection_id           = aws_api_gateway_vpc_link.vpc_link_to_nlb.id
+# }
 
 resource "aws_api_gateway_integration" "proxy" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
