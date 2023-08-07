72c146d8eeac8964d1b93eb852244cccc742b597
Gamiigo <narayutput@gmail.com>
2023-08-07 14:20:49 +0700

    update: module name

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -14,15 +14,15 @@ resource "aws_api_gateway_rest_api" "api" {
   tags = merge(var.tags, { Name = var.configs.name })
 }
 
-resource "aws_api_gateway_resource" "api_resource_proxy" {
+resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "api_method" {
+resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.api_resource_proxy.id
+  resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
   authorization = "NONE"
   request_parameters = {
@@ -30,11 +30,11 @@ resource "aws_api_gateway_method" "api_method" {
   }
 }
 
-resource "aws_api_gateway_integration" "api_integration_proxy" {
+resource "aws_api_gateway_integration" "proxy" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.api_resource_proxy.id
+  resource_id             = aws_api_gateway_resource.proxy.id
   uri                     = var.configs.public_alb_http_tcp_listern_arn
-  http_method             = aws_api_gateway_method.api_method.http_method
+  http_method             = aws_api_gateway_method.proxy.http_method
   type                    = "HTTP_PROXY"
   integration_http_method = "ANY"
   connection_type         = "VPC_LINK"
