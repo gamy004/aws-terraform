939d42e932cd8db364fa23204c903ea2b4758c6d
Gamiigo <narayutput@gmail.com>
2023-08-07 10:49:27 +0700

    update: api gateway `certificate_arn` to `regional_certificate_arn`

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -55,8 +55,8 @@ data "aws_iam_policy_document" "api_access_policy" {
 }
 
 resource "aws_api_gateway_domain_name" "api_domain" {
-  certificate_arn = var.certificate_arn
-  domain_name     = var.domain_name
+  regional_certificate_arn = var.certificate_arn
+  domain_name              = var.domain_name
 
   endpoint_configuration {
     types = ["REGIONAL"]
