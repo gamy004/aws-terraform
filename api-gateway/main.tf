40ce7179941bb29595c08d3bc4576c3afcae475c
Wasuphat Pudjaika <narayutput@gmail.com>
2023-08-07 16:18:25 +0700

    update: base path

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -17,13 +17,13 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "base" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = ""
+  path_part   = "/"
 }
 
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = "/{proxy+}"
 }
 
 resource "aws_api_gateway_method" "base" {
