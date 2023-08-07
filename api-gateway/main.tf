c879654118556e23d2a53aede9976ac9d5c00f3f
Gamiigo <narayutput@gmail.com>
2023-08-07 15:14:42 +0700

    add: https prefix to proxy integration uri

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -33,7 +33,7 @@ resource "aws_api_gateway_method" "proxy" {
 resource "aws_api_gateway_integration" "proxy" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.proxy.id
-  uri                     = var.configs.private_nlb_dns_name
+  uri                     = "https://${var.configs.private_nlb_dns_name}"
   http_method             = aws_api_gateway_method.proxy.http_method
   type                    = "HTTP_PROXY"
   integration_http_method = "GET"
