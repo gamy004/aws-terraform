6e1997e2871d17aa934538c3097161370b3b7440
Wasuphat Pudjaika <narayutput@gmail.com>
2023-08-07 16:10:56 +0700

    update: api gateway resource

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -14,20 +14,44 @@ resource "aws_api_gateway_rest_api" "api" {
   tags = merge(var.tags, { Name = var.configs.name })
 }
 
+resource "aws_api_gateway_resource" "base" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = ""
+}
+
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_method" "base" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "GET"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
   request_parameters = {
     "method.request.path.proxy" = true
   }
+}
+
+resource "aws_api_gateway_integration" "base" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.base.id
+  uri                     = "https://${var.configs.private_nlb_dns_name}/"
+  http_method             = aws_api_gateway_method.base.http_method
+  type                    = "HTTP_PROXY"
+  integration_http_method = "GET"
+  connection_type         = "VPC_LINK"
+  connection_id           = aws_api_gateway_vpc_link.vpc_link_to_nlb.id
 }
 
 resource "aws_api_gateway_integration" "proxy" {
@@ -36,7 +60,7 @@ resource "aws_api_gateway_integration" "proxy" {
   uri                     = "https://${var.configs.private_nlb_dns_name}/{proxy}"
   http_method             = aws_api_gateway_method.proxy.http_method
   type                    = "HTTP_PROXY"
-  integration_http_method = "GET"
+  integration_http_method = "ANY"
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.vpc_link_to_nlb.id
   cache_key_parameters    = ["method.request.path.proxy"]
