dd72e39af01aaa927d7cea925ed7013f49018201
Gamiigo <narayutput@gmail.com>
2023-08-11 16:06:56 +0700

    chore: remove output

--- outputs.tf
+++ outputs.tf
@@ -15,26 +15,26 @@
 #   value = var.backend_configs
 # }
 
-output "service_configs" {
-  value = local.service_configs
-}
+# output "service_configs" {
+#   value = local.service_configs
+# }
 
-output "service_definitions" {
-  value = module.service.service_definitions
-}
+# output "service_definitions" {
+#   value = module.service.service_definitions
+# }
 
-output "repo_configs" {
-  value = module.pipeline.repo_configs
-}
+# output "repo_configs" {
+#   value = module.pipeline.repo_configs
+# }
 
-output "ci_configs" {
-  value = module.pipeline.ci_configs
-}
+# output "ci_configs" {
+#   value = module.pipeline.ci_configs
+# }
 
-output "review_configs" {
-  value = module.pipeline.review_configs
-}
+# output "review_configs" {
+#   value = module.pipeline.review_configs
+# }
 
-output "pipeline_configs" {
-  value = module.pipeline.pipeline_configs
-}
+# output "pipeline_configs" {
+#   value = module.pipeline.pipeline_configs
+# }
