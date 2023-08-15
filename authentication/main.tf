47b4000adad44f8e457cb689f9377f079b21ea12
Wasuphat Pudjaika <narayutput@gmail.com>
2023-08-15 22:28:12 +0700

    add: schema email

--- authentication/main.tf
+++ authentication/main.tf
@@ -16,10 +16,23 @@ resource "aws_cognito_user_pool" "pool" {
     minimum_length = try(var.configs.password_minimum_length, 6)
   }
 
-  auto_verified_attributes = try(var.configs.required_user_attributes, ["email"])
   username_attributes      = try(var.configs.username_attributes, ["email"])
+  auto_verified_attributes = try(var.configs.required_user_attributes, ["email"])
   user_attribute_update_settings {
     attributes_require_verification_before_update = try(var.configs.required_user_attributes, ["email"])
+  }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "email"
+    required                 = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
   }
   #   dynamic "user_attribute_update_settings" {
   #     for_each = length(var.configs.required_user_attributes) > 0 ? [var.configs.required_user_attributes] : []
