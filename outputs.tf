4960bc2134e32086c27a933eb9c7f65e64fb74dd
Wasuphat Pudjaika <narayutput@gmail.com>
2023-08-25 15:31:08 +0700

    test: check output `service_ecs_configs`

--- outputs.tf
+++ outputs.tf
@@ -15,9 +15,9 @@
 #   value = var.backend_configs
 # }
 
-# output "service_configs" {
-#   value = local.service_configs
-# }
+output "service_ecs_configs" {
+  value = local.service_ecs_configs
+}
 
 # output "service_definitions" {
 #   value = module.service.service_definitions
