e259fc6171f61644bf0b22f0c0d2288ef81dd845
Wasuphat Pudjaika <narayutput@gmail.com>
2023-09-06 13:33:57 +0700

    update: change method response back to 200

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -125,7 +125,7 @@ resource "aws_api_gateway_method_response" "proxy_options_method_response" {
   rest_api_id = aws_api_gateway_rest_api.api[each.key].id
   resource_id = aws_api_gateway_resource.proxy[each.key].id
   http_method = aws_api_gateway_method.proxy_options[each.key].http_method
-  status_code = "204"
+  status_code = "200"
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers"     = true
     "method.response.header.Access-Control-Allow-Methods"     = true
